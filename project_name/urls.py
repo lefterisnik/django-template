a93e48ad530e50c2afb8fed142f3702aa5070e01
Lefteris Nikoltsios <lefteris.nikoltsios@gmail.com>
2015-10-14 00:46:42 +0300

    Move admin url outside the i18n.

--- project_name/urls.py
+++ project_name/urls.py
@@ -19,10 +19,10 @@ from loosecms.conf.urls.i18n import simple_i18n_patterns
 
 urlpatterns = [
     url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^admin/', include(admin.site.urls)),
 ]
 
 urlpatterns += simple_i18n_patterns(
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('loosecms.urls')),
 )
 
